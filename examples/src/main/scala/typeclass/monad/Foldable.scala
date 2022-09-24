33bb7217ffa44a8a7cfa1a54d0e05270f4fa3687
Артём <artemkorsakov@mail.ru>
2022-09-24 16:57:05 +0300

    Add FoldableSuite

--- examples/src/main/scala/typeclass/monad/Foldable.scala
+++ examples/src/main/scala/typeclass/monad/Foldable.scala
@@ -1,5 +1,49 @@
 package typeclass.monad
 
-trait Foldable {
+import typeclass.common.*
 
-}
+trait Foldable[F[_]]:
+  extension [A](fa: F[A]) def foldRight[B](init: B)(f: (A, B) => B): B
+
+object Foldable:
+  given idFoldable: Foldable[Id] with
+    extension [A](fa: Id[A])
+      override def foldRight[B](init: B)(f: (A, B) => B): B =
+        f(fa.value, init)
+
+  given optionFoldable: Foldable[Option] with
+    extension [A](fa: Option[A])
+      override def foldRight[B](init: B)(f: (A, B) => B): B =
+        fa match
+          case Some(a) => f(a, init)
+          case None    => init
+
+  given listFoldable: Foldable[List] with
+    extension [A](as: List[A])
+      override def foldRight[B](init: B)(f: (A, B) => B): B =
+        as.foldRight(init)(f)
+
+  given tuple2Foldable: Foldable[[X] =>> (X, X)] with
+    extension [A](fa: (A, A))
+      override def foldRight[B](init: B)(f: (A, B) => B): B =
+        val (a0, a1) = fa
+        val b = f(a1, init)
+        f(a0, b)
+
+  given tuple3Foldable: Foldable[[X] =>> (X, X, X)] with
+    extension [A](fa: (A, A, A))
+      override def foldRight[B](init: B)(f: (A, B) => B): B =
+        val (a0, a1, a2) = fa
+        val b0 = f(a2, init)
+        val b1 = f(a1, b0)
+        f(a0, b1)
+
+  given eitherFoldable[E]: Foldable[[x] =>> Either[E, x]] with
+    extension [A](fa: Either[E, A])
+      override def foldRight[B](init: B)(f: (A, B) => B): B =
+        fa match
+          case Right(a) => f(a, init)
+          case Left(_)  => init
+
+  def foldRight[F[_], A, B](fa: F[A])(init: B)(f: (A, B) => B)(using foldable: Foldable[F]): B =
+    foldable.foldRight(fa)(init)(f)
--- examples/src/test/scala/typeclass/monad/FoldableSuite.scala
+++ examples/src/test/scala/typeclass/monad/FoldableSuite.scala
@@ -1,5 +1,51 @@
 package typeclass.monad
 
-class FoldableSuite {
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop.*
+import typeclass.common.*
+import typeclass.monad.Foldable.{foldRight, given}
 
-}
+class FoldableSuite extends ScalaCheckSuite:
+  property("'Обертку' можно агрегировать") {
+    forAll { (x: Int) =>
+      assertEquals(foldRight(Id(x))(100)(_ + _), 100 + x)
+    }
+  }
+
+  property("'Option' можно агрегировать") {
+    forAll { (maybeInt: Option[Int]) =>
+      val expected = maybeInt match
+        case Some(a) => a + 100
+        case None    => 100
+      assertEquals(foldRight(maybeInt)(100)(_ + _), expected)
+    }
+  }
+
+  property("'List' можно агрегировать") {
+    forAll { (list: List[Int]) =>
+      assertEquals(foldRight(list)(100)(_ + _), list.sum + 100)
+    }
+  }
+
+  property("'Tuple2' можно агрегировать") {
+    forAll { (x: Int, y: Int) =>
+      val actual = foldRight[[X] =>> (X, X), Int, Int]((x, y))(100)(_ + _)
+      assertEquals(actual, x + y + 100)
+    }
+  }
+
+  property("'Tuple3' можно агрегировать") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      val actual = foldRight[[X] =>> (X, X, X), Int, Int]((x, y, z))(100)(_ + _)
+      assertEquals(actual, x + y + z + 100)
+    }
+  }
+
+  property("'Either' можно агрегировать") {
+    forAll { (either: Either[String, Int]) =>
+      val expected = either match
+        case Right(a) => a + 100
+        case _        => 100
+      assertEquals(foldRight(either)(100)(_ + _), expected)
+    }
+  }

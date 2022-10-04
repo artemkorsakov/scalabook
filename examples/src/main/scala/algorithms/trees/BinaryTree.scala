caa41a9b8acee0f856b176510bbe5176119a9a83
Артём <artemkorsakov@mail.ru>
2022-10-04 18:29:22 +0300

    Algorithms

--- examples/src/main/scala/algorithms/trees/BinaryTree.scala
+++ examples/src/main/scala/algorithms/trees/BinaryTree.scala
@@ -12,26 +12,22 @@ enum BinaryTree[+A]:
     case Leaf            => 0
     case Branch(_, l, r) => 1 + l.depth.max(r.depth)
 
+  lazy val preorder: IndexedSeq[A] = this match
+    case Leaf                       => IndexedSeq.empty
+    case Branch(value, left, right) => IndexedSeq(value) ++ left.preorder ++ right.preorder
+
+  lazy val inorder: IndexedSeq[A] = this match
+    case Leaf                       => IndexedSeq.empty
+    case Branch(value, left, right) => left.inorder ++ IndexedSeq(value) ++ right.inorder
+
+  lazy val postorder: IndexedSeq[A] = this match
+    case Leaf                       => IndexedSeq.empty
+    case Branch(value, left, right) => left.postorder ++ right.postorder ++ IndexedSeq(value)
+
 object BinaryTree:
   def apply[A](list: List[A]): BinaryTree[A] =
     list match
       case Nil => Leaf
       case x :: xs =>
-        val (leftList, rightList) = xs.splitAt(xs.length / 2)
-        Branch(x, BinaryTree(leftList), BinaryTree(rightList))
-
-  def preorder[A](binTree: BinaryTree[A]): IndexedSeq[A] =
-    binTree match
-      case Leaf                       => IndexedSeq.empty
-      case Branch(value, left, right) => IndexedSeq(value) ++ preorder(left) ++ preorder(right)
-
-  def inorder[A](binTree: BinaryTree[A]): IndexedSeq[A] =
-    binTree match
-      case Leaf                                   => IndexedSeq.empty
-      case Branch(value, leftBranch, rightBranch) => inorder(leftBranch) ++ IndexedSeq(value) ++ inorder(rightBranch)
-
-  def postorder[A](binTree: BinaryTree[A]): IndexedSeq[A] =
-    binTree match
-      case Leaf => IndexedSeq.empty
-      case Branch(value, leftBranch, rightBranch) =>
-        postorder(leftBranch) ++ postorder(rightBranch) ++ IndexedSeq(value)
+        val (left, right) = xs.splitAt(xs.length / 2)
+        Branch(x, BinaryTree(left), BinaryTree(right))
---
+++ examples/src/test/scala/algorithms/trees/BinaryTreeOperationSuite.scala
@@ -0,0 +1,23 @@
+package algorithms.trees
+
+import algorithms.trees.BinaryTree.{Branch, Leaf}
+import munit.FunSuite
+
+class BinaryTreeOperationSuite extends FunSuite:
+  private val binaryTree: BinaryTree[Int] = Branch(
+    5,
+    Branch(4, Branch(1, Branch(0, Leaf, Leaf), Branch(2, Leaf, Leaf)), Branch(3, Leaf, Leaf)),
+    Branch(6, Leaf, Leaf)
+  )
+
+  test("BinaryTree.preorder") {
+    assertEquals(binaryTree.preorder, Vector(5, 4, 1, 0, 2, 3, 6))
+  }
+
+  test("BinaryTree.inorder") {
+    assertEquals(binaryTree.inorder, Vector(0, 1, 2, 4, 3, 5, 6))
+  }
+
+  test("BinaryTree.postorder") {
+    assertEquals(binaryTree.postorder, Vector(0, 2, 1, 3, 4, 6, 5))
+  }

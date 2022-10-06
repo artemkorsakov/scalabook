fbce083d985a3ca1f72968e55834594a01c07937
Артём <artemkorsakov@mail.ru>
2022-10-06 20:33:58 +0300

    ScalaZ

--- examples/src/main/scala/typeclass/monad/Free.scala
+++ examples/src/main/scala/typeclass/monad/Free.scala
@@ -3,7 +3,7 @@ package typeclass.monad
 import scala.annotation.tailrec
 
 sealed trait Free[F[_], A]
-final case class Unit[F[_], A](a: A) extends Free[F, A]
+final case class UnitF[F[_], A](a: A) extends Free[F, A]
 final case class FlatMap[F[_], A, B](a: Free[F, A], fx: A => Free[F, B]) extends Free[F, B]
 final case class LiftF[F[_], A](fn: F[A]) extends Free[F, A]
 
@@ -14,10 +14,10 @@ object Free:
   given freeFunctor[F[_]]: Functor[[X] =>> Free[F, X]] with
     extension [A](as: Free[F, A])
       override def map[B](f: A => B): Free[F, B] =
-        FlatMap(as, a => Unit(f(a)))
+        FlatMap(as, a => UnitF(f(a)))
 
   given freeMonad[F[_]]: Monad[[X] =>> Free[F, X]] with
-    override def unit[A](a: => A): Free[F, A] = Unit(a)
+    override def unit[A](a: => A): Free[F, A] = UnitF(a)
 
     extension [A](fa: Free[F, A]) override def flatMap[B](f: A => Free[F, B]): Free[F, B] = FlatMap(fa, f)
 
@@ -25,8 +25,8 @@ object Free:
 
   def foldF[F[_], M[_], A](fa: Free[F, A])(trans: Natural[F, M])(using monad: Monad[M]): M[A] =
     fa match
-      case Unit(a)                   => monad.unit(a)
+      case UnitF(a)                   => monad.unit(a)
       case LiftF(fn)                 => trans.transform(fn)
-      case FlatMap(Unit(a), f)       => foldF(f(a))(trans)
+      case FlatMap(UnitF(a), f)       => foldF(f(a))(trans)
       case FlatMap(LiftF(a), f)      => monad.flatMap(trans.transform(a)) { aa => foldF(f(aa))(trans) }
       case FlatMap(FlatMap(a, f), g) => foldF(FlatMap(a, aa => FlatMap(f(aa), g)))(trans)

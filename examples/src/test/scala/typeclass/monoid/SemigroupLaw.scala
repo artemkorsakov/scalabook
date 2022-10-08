563e331a3a5d66eb3613ef9d3ab8b057be430a33
Артём <artemkorsakov@mail.ru>
2022-10-08 18:14:44 +0300

    ScalaZ

--- examples/src/test/scala/typeclass/monoid/SemigroupLaw.scala
+++ examples/src/test/scala/typeclass/monoid/SemigroupLaw.scala
@@ -5,7 +5,4 @@ import typeclass.monoid.Semigroup.combine
 
 trait SemigroupLaw extends Assertions:
   def checkSemigroupLaw[A](x: A, y: A, z: A)(using Semigroup[A]): Unit =
-    checkAssociativity(x, y, z)
-
-  private def checkAssociativity[A](x: A, y: A, z: A)(using Semigroup[A]): Unit =
     assertEquals(combine(combine(x, y), z), combine(x, combine(y, z)), "Associativity")

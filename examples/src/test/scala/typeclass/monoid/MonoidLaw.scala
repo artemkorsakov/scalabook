94d288c1cecd5b6ac80adb1029e1d9525e4fa982
Артём <artemkorsakov@mail.ru>
2022-10-08 18:23:54 +0300

    ScalaZ

--- examples/src/test/scala/typeclass/monoid/MonoidLaw.scala
+++ examples/src/test/scala/typeclass/monoid/MonoidLaw.scala
@@ -5,11 +5,5 @@ import typeclass.monoid.Monoid.{combine, empty}
 trait MonoidLaw extends SemigroupLaw:
   def checkMonoidLaw[A](x: A, y: A, z: A)(using Monoid[A]): Unit =
     checkSemigroupLaw(x, y, z)
-    checkRightIdentity(x)
-    checkLeftIdentity(x)
-
-  private def checkRightIdentity[A](x: A)(using Monoid[A]): Unit =
     assertEquals(combine(x, empty), x, "right identity")
-
-  private def checkLeftIdentity[A](x: A)(using Monoid[A]): Unit =
     assertEquals(combine(empty, x), x, "left identity")

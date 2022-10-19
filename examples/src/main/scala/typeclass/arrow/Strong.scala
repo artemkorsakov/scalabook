47523ad42b7de89c68138f5fd1b27579f7733d86
Артём <artemkorsakov@mail.ru>
2022-10-19 20:10:06 +0300

    ScalaZ

--- examples/src/main/scala/typeclass/arrow/Strong.scala
+++ examples/src/main/scala/typeclass/arrow/Strong.scala
@@ -1,5 +1,7 @@
 package typeclass.arrow
 
-trait Strong {
+trait Strong[=>:[_, _]] extends Profunctor[=>:]:
+  def first[A, B, C](fa: A =>: B): (A, C) =>: (B, C)
 
-}
+  def second[A, B, C](fa: A =>: B): (C, A) =>: (C, B) =
+    dimap[(A, C), (B, C), (C, A), (C, B)](first(fa))(_.swap)(_.swap)

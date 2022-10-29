21f11e7fcd5b9290d9df81d27df95c781ea8d2da
Артём <artemkorsakov@mail.ru>
2022-10-29 14:12:48 +0300

    ScalaZ

--- examples/src/test/scala/typeclass/bifunctor/BitraverseLaw.scala
+++ examples/src/test/scala/typeclass/bifunctor/BitraverseLaw.scala
@@ -12,9 +12,14 @@ trait BitraverseLaw extends TraverseLaw, BifoldableLaw, BifunctorLaw:
       far: F[A, R],
       fla: F[L, A],
       faa: F[A, A]
-  )(using f: A => B, fReverse: B => A, g: B => C, gReverse: C => B)(using Monoid[Vector[A]])(using
+  )(using f: A => B, fReverse: B => A, g: B => C, gReverse: C => B)(using m: Monoid[Vector[A]])(using
       bi: Bitraverse[F]
   ): Unit =
     checkBifoldableLaw[F, L, R, A](far, fla, faa)
     checkBifunctorLaw[F, L, R, A, B, C](far, fla, faa)
-    // checkTraverseLaw[F[_] : Traverse, G, H, A, B, C](fa)
+    given t1: Traverse[[X] =>> F[X, R]] = bi.leftTraverse
+    given t2: Traverse[[X] =>> F[L, X]] = bi.rightTraverse
+    given t3: Traverse[[X] =>> F[X, X]] = bi.uTraverse
+    checkTraverseLaw[[X] =>> F[X, R], G, H, A, B, C](far)
+    checkTraverseLaw[[X] =>> F[L, X], G, H, A, B, C](fla)
+    checkTraverseLaw[[X] =>> F[X, X], G, H, A, B, C](faa)
